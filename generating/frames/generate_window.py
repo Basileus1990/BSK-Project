86ca6ebaebc898a2fb486bfb71907d645004f0ea
Wiesiek <jakubwisniewski1h@gmail.com>
2025-06-05 13:51:02 +0200

    Comments in generate_window.py

--- generating/frames/generate_window.py
+++ generating/frames/generate_window.py
@@ -1,6 +1,6 @@
 ##
 # @file generate_window.py
-# @brief GUI window for generating RSA key pairs
+# @brief GUI window for generating RSA key pairs.
 # @details Provides input fields for taking a path to saving public/private keys, setting 4-digit PIN and a progress bar to display the current status of the generating process.
 #
 import time
@@ -10,12 +10,12 @@ from tkinter import filedialog, ttk
 from generating.key_generate.RSA_key_generator import generate_keys
 from generating.key_generate.AES_key_generator import aes_encrypt_file
 
-## @var PRIVATE_KEY_NAME
-#  @brief Default filename for the private key
+## @var PRIVATE_KEY_NAME.
+#  @brief Default filename for the private key.
 PRIVATE_KEY_NAME = "private_key.key"
 
-## @var PUBLIC_KEY_NAME
-#  @brief Default filename for the public key
+## @var PUBLIC_KEY_NAME.
+#  @brief Default filename for the public key.
 PUBLIC_KEY_NAME = "public_key.key"
 
 FOREGROUND_COLOR = "#ffffff"
@@ -46,6 +46,11 @@ class GenerateKeys(tk.Frame):
         self.show_pin()
         self.show_progress_bar()
 
+    ##
+    # @brief Show header label.
+    #
+    # @details The function display on the top of window title text.
+    #
     def show_header_label(self):
         # Header label
         self.header_label = tk.Label(
@@ -58,6 +63,11 @@ class GenerateKeys(tk.Frame):
         )
         self.header_label.pack(pady=(0, 20))
 
+    ##
+    # @brief Show the path field.
+    #
+    # @details The function display the section title, text field with the public/private keys generation location and buttons to open window for choosing a final destinations.
+    #
     def show_path_field(self):
         # Header path selection
         self.path_label = tk.Label(
@@ -110,6 +120,11 @@ class GenerateKeys(tk.Frame):
         )
         self.button_explore_private.pack(padx=5, pady=(0, 5), anchor="center")
 
+    ##
+    # @brief Show the PIN field.
+    #
+    # @details The function display the section label and text field to get a 4-digit PIN.
+    #
     def show_pin(self):
         self.label_pin = tk.Label(self, text="PIN:", fg=FOREGROUND_COLOR, bg=BACKGROUND_COLOR)
         self.label_pin.pack(anchor='center', padx=5)
@@ -117,6 +132,11 @@ class GenerateKeys(tk.Frame):
         self.pin_entry.pack(padx=5, pady=(0, 10), anchor="center")
         self.pin_entry.pack(padx=5, pady=5)
 
+    ##
+    # @brief Show the progress bar
+    #
+    # @details The function display the progress bar label with description of the current status and bar to display graphical representation of progress
+    #
     def show_progress_bar(self):
         # Progress bar
         self.progress = ttk.Progressbar(self, orient="horizontal", length=300, mode="determinate",style="green.Horizontal.TProgressbar")
@@ -126,6 +146,11 @@ class GenerateKeys(tk.Frame):
         self.progress["value"] = 0
         self.progress.pack(pady=(0, 10))
 
+    ##
+    # @brief Show the generate button.
+    #
+    # @details The function display the button to start of the generation function.
+    #
     def show_generate_button(self):
         # Generate keys button
         self.button_generate = tk.Button(
@@ -140,7 +165,13 @@ class GenerateKeys(tk.Frame):
         )
         self.button_generate.pack(padx=5, pady=(0, 5), anchor="center")
 
-    # Open file dialog for choosing folder
+    ##
+    # @brief Open external window to choose a folder.
+    #
+    # @details This function open external window, where you might choose a folder.
+    #
+    # @param entry Path, where we save the chosen location.
+    #
     def open_folder(self, entry):
         folder = filedialog.askdirectory(title="Select a public key")
 
@@ -148,7 +179,15 @@ class GenerateKeys(tk.Frame):
             entry.delete(0, tk.END)
             entry.insert(0, folder)
 
-    # generate a pair of keys in RSA
+    ##
+    # @brief Manager to generation of keys.
+    #
+    # @details The function are the simple manager to entry correctly params for generate_keys_thread function, which is running on the other thread.
+    #
+    # @param public_path  Path to the public key.
+    # @param private_path Path to the private key.
+    # @param pin Code PIN to encrypt private key.
+    #
     def generate_keys_manager(self, public_path: str, private_path: str, pin: str):
         self.update_status("Started...", 0, "green.Horizontal.TProgressbar")
         if not pin.isdigit() or len(pin) != 4:
@@ -166,6 +205,17 @@ class GenerateKeys(tk.Frame):
 
         threading.Thread(target=self.generate_keys_thread, args=(public_path, private_path, pin)).start()
 
+    ##
+    # @brief Generate keys status function in the thread.
+    #
+    # @details This function calls the generate_keys function from RSA_key_generator and the aes_encrypt_file function from AES_key_generator to generate a public/private RSA key pairs
+    # and encrypt the private key using a 4-digit PIN code.
+    # Additionally, it updates the progress bar to reflect the current stage of the operation.
+    #
+    # @param public_path  Path to the public key.
+    # @param private_path Path to the private key.
+    # @param pin Code PIN to encrypt private key.
+    #
     def generate_keys_thread(self, public_path: str, private_path: str, pin: str):
         self.update_status("Generating RSA keys...", 25,"green.Horizontal.TProgressbar")
         time.sleep(0.1)
@@ -188,6 +238,15 @@ class GenerateKeys(tk.Frame):
         else:
             self.update_status("Private key encryption failed", 100,"red.Horizontal.TProgressbar")
 
+    ##
+    # @brief Update status of the progress bar
+    #
+    # @details This function changes the status bar label, value and its style.
+    #
+    # @param message Text message to be displayed in the status label above the progress bar.
+    # @param progress Integer value (0-100) indicating the completion percentage of the progress bar.
+    # @param style TTK style name to apply to the progress bar.
+    #
     def update_status(self, message: str, progress: int, style: str):
         def update():
             self.status_label.configure(text=message)
